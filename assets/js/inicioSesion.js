6e65a91d71198099380bc63b5b28a818fc3ab84e
Masterx43 <bastiancito.26@gmail.com>
2025-09-22 19:53:12 -0300

    Verificacion de inicio de sesion

--- assets/js/inicioSesion.js
+++ assets/js/inicioSesion.js
@@ -1,31 +1,13 @@
-document.getElementById("formulario").addEventListener("submit", function(e){
-    e.preventDefault(); 
+document.getElementById("formulario").addEventListener("submit", function(e) {
+    e.preventDefault();
 
-    // Capturar valores
-    let nombre = document.getElementById("text1").value.trim();
-    let apellido = document.getElementById("textApellido").value.trim();
     let email = document.getElementById("txtEmail").value.trim();
+    let pass = document.getElementById("password").value
 
-    // Labels de error
-    let errorNombre = document.getElementById("lblErrorNombre");
-    let errorApellido = document.getElementById("lblErrorApellido");
     let errorEmail = document.getElementById("lblErrorEmail");
+    let errorPass = document.getElementById("lblErrorPassword");
 
-    let vefNombre = false, vefApellido = false, vefCorreo = false;
-
-    if(nombre === "" || nombre.length < 3){
-        errorNombre.innerText = "El nombre es obligatorio y debe tener al menos 3 caracteres";
-    } else {
-        errorNombre.innerText = "";
-        vefNombre = true;
-    }
-
-    if(apellido === "" || apellido.length < 3){
-        errorApellido.innerText = "El apellido es obligatorio y debe tener al menos 3 caracteres";
-    } else {
-        errorApellido.innerText = "";
-        vefApellido = true;
-    }
+    let vefCorreo = false, vefPass = false;
 
     if(email === ""){
         errorEmail.innerText = "El correo no puede estar vacío";
@@ -36,15 +18,23 @@ document.getElementById("formulario").addEventListener("submit", function(e){
         vefCorreo = true;
     }
 
-    if (vefNombre && vefApellido && vefCorreo) {
-        let usuario = { nombre, apellido, email };
-        alert("Formulario enviado correctamente");
-        console.log(usuario);
-
-        // Redirección a otra página
-        window.location.href = "index.html"; 
+    if(pass === ""){
+        errorPass.innerText = "La contraseña no puede estar vacía";
+    } else {
+        errorPass.innerText = "";
+        vefPass = true;
     }
 
-    
+    if(vefCorreo && vefPass){
+        // Recuperar usuario registrado
+        let usuarioGuardado = JSON.parse(localStorage.getItem("usuario"));
+
+        if(usuarioGuardado.email === email && usuarioGuardado.pass === pass){
+            alert("Bienvenido " + usuarioGuardado.nombre);
+            window.location.href = "index.html";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    }
 });
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,15 @@ document.getElementById("formulario").addEventListener("submit", function(e){
     let nombre = document.getElementById("text1").value.trim();
     let apellido = document.getElementById("textApellido").value.trim();
     let email = document.getElementById("txtEmail").value.trim();
+    let pass = document.getElementById("password").value
 
     // Labels de error
     let errorNombre = document.getElementById("lblErrorNombre");
     let errorApellido = document.getElementById("lblErrorApellido");
     let errorEmail = document.getElementById("lblErrorEmail");
+    let passError = document.getElementById("lblErrorPassword");
 
-    let vefNombre = false, vefApellido = false, vefCorreo = false;
+    let vefNombre = false, vefApellido = false, vefCorreo = false, vefPass = false;
 
     if(nombre === "" || nombre.length < 3){
         errorNombre.innerText = "El nombre es obligatorio y debe tener al menos 3 caracteres";
@@ -36,15 +38,22 @@ document.getElementById("formulario").addEventListener("submit", function(e){
         vefCorreo = true;
     }
 
-    if (vefNombre && vefApellido && vefCorreo) {
-        let usuario = { nombre, apellido, email };
-        alert("Formulario enviado correctamente");
-        console.log(usuario);
+    if(pass === "") {
+        passError.innerText = "No puede estar vacio este campo"
+    } else {
+        passError.innerText = "";
+        vefPass = true
+    }
 
-        // Redirección a otra página
+    if (vefNombre && vefApellido && vefCorreo && vefPass) {
+        let usuario = { nombre, apellido, email , pass};
+
+        localStorage.setItem("usuario", JSON.stringify(usuario));
+        
+        alert("Usuario registrado correctamente");
+        console.log(usuario);
         window.location.href = "index.html"; 
     }
 
-    
 });
 
